f6d16deec1a30fbb298d1894067872e0e1ca0778
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-02-21 11:47:02 +0100

    First commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+
+export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+}
